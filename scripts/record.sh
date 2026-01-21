b9b588dfacd67489c9600a16052a2266af3aa805
sorel <sorel.ferris@gmail.com>
2026-01-21 22:44:20 +0800

    feat: update camera index in record script and add merge_datasets script

--- scripts/record.sh
+++ scripts/record.sh
@@ -5,11 +5,12 @@ lerobot-record \
     --robot.type=so101_follower \
     --robot.port=/dev/ttyACM1 \
     --robot.id=my_awesome_follower_arm \
-    --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, wrist: {type: opencv, index_or_path: 4, width: 640, height: 480, fps: 30}}" \
+    --robot.cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, wrist: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 30}}" \
     --teleop.type=so101_leader \
     --teleop.port=/dev/ttyACM0 \
     --teleop.id=my_awesome_leader_arm \
     --display_data=true \
-    --dataset.repo_id="sorel/record-0120" \
-    --dataset.num_episodes=50 \
-    --dataset.single_task="Grab the tape and put it in the box."
+    --dataset.repo_id="sorel/record-0121-p1" \
+    --dataset.num_episodes=10 \
+    --dataset.single_task="Grab the tape and put it in the box." \
+    --dataset.push_to_hub=false
