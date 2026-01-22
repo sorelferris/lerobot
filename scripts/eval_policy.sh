bdb7686736acae3a33af93a7d156f931ba7ce23a
sorel <sorel.ferris@gmail.com>
2026-01-22 22:17:49 +0800

    fix: update robot type and enable display data in eval_policy script

--- scripts/eval_policy.sh
+++ scripts/eval_policy.sh
@@ -15,15 +15,12 @@ lerobot-record  \
   --robot.port=/dev/ttyACM1 \
   --robot.cameras="$cameras" \
   --robot.id=my_awesome_follower_arm \
-  --display_data=false \
+  --display_data=true \
   --dataset.repo_id="sorel/eval_so101" \
   --dataset.push_to_hub=false \
   --dataset.single_task="$task" \
-  --dataset.rename_map="$rename_map" \
   --dataset.episode_time_s=3600 \
   --teleop.type=so101_leader \
   --teleop.port=/dev/ttyACM0 \
   --teleop.id=my_awesome_leader_arm \
-  --policy.path="sorel/smolvla-so101-0121"
-
-#   --policy.path="sorel/act_so101-record-0121"
+  --policy.path="sorel/act_so101-record-0121"
