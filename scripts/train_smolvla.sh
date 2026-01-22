bb3b4a7c26d4c133f8f8e817d7228ca46a047a1a
sorel <sorel.ferris@gmail.com>
2026-01-22 15:50:22 +0800

    feat: add rename_map parameter to train_smolvla script for image observation mapping

--- scripts/train_smolvla.sh
+++ scripts/train_smolvla.sh
@@ -6,9 +6,11 @@ data_name="${repo_id#*/}"
 policy_type="smolvla"
 policy_repo_id="sorel/smolvla-${data_name}"
 
+rename_map='{"observation.images.front": "observation.images.camera1", "observation.images.wrist": "observation.images.camera2"}'
 
 lerobot-train \
   --dataset.repo_id=${repo_id} \
+  --rename_map="${rename_map}" \
   --policy.path=lerobot/smolvla_base \
   --policy.repo_id=${policy_repo_id} \
   --policy.device=cuda \
