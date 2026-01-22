217e63d948789f6e08e6a3a9e654076691006b28
sorel <sorel.ferris@gmail.com>
2026-01-22 22:47:32 +0800

    fix: update display_data flag and add rename_map for dataset in eval_policy script

--- scripts/eval_policy.sh
+++ scripts/eval_policy.sh
@@ -5,19 +5,25 @@ task="Grab the tape and put it in the box."
 cameras="{ front: {type: opencv, index_or_path: 0, width: 640, height: 480, fps: 30}, \
     wrist: {type: opencv, index_or_path: 6, width: 640, height: 480, fps: 30}}"
 
+rename_map='{"observation.images.front": "observation.images.camera1", "observation.images.wrist": "observation.images.camera2"}'
+
 rm -rf "/home/sorel/.cache/huggingface/lerobot/sorel/eval_so101"
+
 
 lerobot-record  \
   --robot.type=so101_follower \
   --robot.port=/dev/ttyACM1 \
   --robot.cameras="$cameras" \
   --robot.id=my_awesome_follower_arm \
-  --display_data=true \
+  --display_data=false \
   --dataset.repo_id="sorel/eval_so101" \
   --dataset.push_to_hub=false \
   --dataset.single_task="$task" \
+  --dataset.rename_map="$rename_map" \
   --dataset.episode_time_s=3600 \
   --teleop.type=so101_leader \
   --teleop.port=/dev/ttyACM0 \
   --teleop.id=my_awesome_leader_arm \
-  --policy.path="sorel/act_so101-record-0121"
+  --policy.path="sorel/smolvla-so101-0121"
+
+#   --policy.path="sorel/act_so101-record-0121"
