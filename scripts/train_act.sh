0ca653aaa93584f1104882cd09d96cffed0fa5da
cong.bao <cong.bao@keboda.ai>
2026-03-30 16:44:54 +0800

    fix(script): add missing dataset root path and improve LD_LIBRARY_PATH export

--- scripts/train_act.sh
+++ scripts/train_act.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+
 dataset="$1"
 
 if [ -z "$dataset" ]; then
@@ -7,11 +8,14 @@ if [ -z "$dataset" ]; then
     exit 1
 fi
 
+export LD_LIBRARY_PATH=$CONDA_PREFIX/lib:$LD_LIBRARY_PATH
+
 policy_type="act"
 
 lerobot-train \
   --job_name="train-${policy_type}-${dataset}" \
   --dataset.repo_id="sorel/${dataset}" \
+  --dataset.root="data/train_data/lerobot_v3.0/${dataset}" \
   --policy.type=${policy_type} \
   --policy.repo_id="sorel/${policy_type}-${dataset}" \
   --policy.device=cuda \
